005582e5feb0b7cab9af3c4d562e4845e4a1d936
Subhankar Patra <subhankarpatra2118@gmail.com>
2025-07-03 23:22:14 +0530

    Hero Section done with all page responsiveness

---
+++ src/sections/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <div className="fixed top-3 left-0 w-full flex justify-center items-center z-50">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item">Home</a>
+        <a href="#" className="nav-item">Projects</a>
+        <a href="#" className="nav-item">About</a>
+        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+      </nav>
+    </div>
+  );
+};
+
