22b3cdc258f39e8cb3d5ad0e8c65ab80d4c75ef8
Subhankar Patra <subhankarpatra2118@gmail.com>
2025-07-25 23:25:42 +0530

    Hero Section animation done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,79 +1,93 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
-import ArrowDown from '@/assets/icons/arrow-down.svg';
-import grainImage from '@/assets/images/grain.jpg';
-import StarIcon from '@/assets/icons/star.svg';
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
 import { HeroOrbit } from "@/components/HeroOrbit";
-import SparkelIcon from '@/assets/icons/sparkle.svg';
+import SparkelIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
-      <div className="absolute inset-0 -z-30 opacity-5" style={{backgroundImage: `url(${grainImage.src})`}}></div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
-      <HeroOrbit size={800} rotation={-72}>
-        <StarIcon className="size-28 text-emerald-300"/>
-      </HeroOrbit>
-      <HeroOrbit size={550} rotation={20}>
-        <StarIcon className="size-12 text-emerald-300"/>
-      </HeroOrbit>
-      <HeroOrbit size={590} rotation={98}>
-        <StarIcon className="size-8 text-emerald-300"/>
-      </HeroOrbit>
-      <HeroOrbit size={430} rotation={-14}>
-        <SparkelIcon className="size-8 text-emerald-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={440} rotation={80}>
-        <SparkelIcon className="size-5 text-emerald-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={530} rotation={178}>
-        <SparkelIcon className="size-10 text-emerald-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={710} rotation={144}>
-        <SparkelIcon className="size-14 text-emerald-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={721} rotation={85}>
-        <div className="size-3 bg-emerald-300/20 rounded-full"/>
-      </HeroOrbit>
-      <HeroOrbit size={520} rotation={-43}>
-        <div className="size-2 bg-emerald-300/20 rounded-full"/>
-      </HeroOrbit>
-      <HeroOrbit size={650} rotation={-5}>
-        <div className="size-3 bg-emerald-300/20 rounded-full"/>
-      </HeroOrbit>
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{ backgroundImage: `url(${grainImage.src})` }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        {/* shouldOrbit?: boolean;
+          orbitDuration?: string;
+          shouldSpin?: boolean;
+          spinDuration?: string; */}
+        <HeroOrbit size={430} rotation={-14} shouldOrbit orbitDuration="30s" shouldSpin spinDuration="7s">
+          <SparkelIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={80} shouldOrbit orbitDuration="32s" shouldSpin spinDuration="7s">
+          <SparkelIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-43} shouldOrbit orbitDuration="34s" shouldSpin spinDuration="7s">
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178} shouldOrbit orbitDuration="36s" shouldSpin spinDuration="7s">
+          <SparkelIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+
+        <HeroOrbit size={550} rotation={20} shouldOrbit orbitDuration="38s" shouldSpin spinDuration="7s">
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98} shouldOrbit orbitDuration="40s" shouldSpin spinDuration="7s">
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s" shouldSpin spinDuration="7s">
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+
+        <HeroOrbit size={710} rotation={144} shouldOrbit orbitDuration="44s" shouldSpin spinDuration="7s">
+          <SparkelIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={721} rotation={85} shouldOrbit orbitDuration="46s" shouldSpin spinDuration="7s">
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+
+        <HeroOrbit size={800} rotation={-72} shouldOrbit orbitDuration="48s" shouldSpin spinDuration="7s">
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
       </div>
       <div className="container">
         <div className="flex flex-col items-center gap-0">
-          <Image 
-            src={memojiImage} 
-            className="size-[100px] block mx-auto" 
-            alt="Person peeking from behind laptop" 
+          <Image
+            src={memojiImage}
+            className="size-[100px] block mx-auto"
+            alt="Person peeking from behind laptop"
           />
 
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 rounded-lg inline-flex items-center gap-4 ">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">Available for new Projects</div>
+            <div className="bg-green-500 size-2.5 rounded-full relative">
+              <div className="bg-green-500 absolute inset-0 animate-ping-large rounded-full"></div>
+            </div>
+            <div className="text-sm font-medium">
+              Available for new Projects
+            </div>
           </div>
         </div>
         <div className="max-w-lg mx-auto">
-
-        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-          Building Exceptional User Experiences
-        </h1>
-        <p className="mt-4 text-center text-white/60 md:text-lg">
-          I specialize in transforming designs into functional, high-performing web applications. Let's discuss your next project.
-        </p>
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let's discuss your next project.
+          </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
           </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">  
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
             <span>👋</span>
             <span className="font-semibold">Let's Connect</span>
           </button>
@@ -82,4 +96,3 @@ export const HeroSection = () => {
     </div>
   );
 };
-
