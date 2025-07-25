2f559278f121f3f84f1e264695115dc8e6855408
Subhankar Patra <subhankarpatra2118@gmail.com>
2025-07-25 19:42:23 +0530

    contact section done

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,38 @@
+import ArrowRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let's create something amazing together
+              </h2>
+              <p className="text-sm md:text-base mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                dolorem cum quos eaque ab officiis laboriosam facilis?
+                Voluptatibus doloremque laboriosam dolorum, quasi delectus iste
+                porro consectetur omnis tempora, ab corporis?
+              </p>
+            </div>
+            <div>
+              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8 w-max border border-gray-950">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowRightIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
